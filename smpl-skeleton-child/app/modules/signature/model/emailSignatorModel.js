01c80d64435db56fc32e960d76e860450f51033f
Vasyl Zhuk <vasiliy@boylesoftware.com>
2015-03-04 02:07:14 +0200

    Translated form validation messages

--- smpl-skeleton-child/app/modules/signature/model/emailSignatorModel.js
+++ smpl-skeleton-child/app/modules/signature/model/emailSignatorModel.js
@@ -9,45 +9,68 @@ define (
 
 		return Backbone.Model.extend ({
 			validation: {
-				Name	: {
+				Name	: [{
 					required: true,
-					maxLength	: 255,
+					msg: 'Необхідно вказати ім’я'
 				},
+					{
+						maxLength	: 255,
+						msg: 'Ім’я надто довге (максимум 255 символів)'
+					}],
 				'Middle-Name' : {
 					required	: false,
-					maxLength	: 255
+					maxLength	: 255,
+					msg: 'Значення надто довге (максимум 255 символів)'
 				},
 				Surname : {
 					required	: false,
 					maxLength	: 255,
+					msg: 'Значення надто довге (максимум 255 символів)'
 				},
-				Email	: {
-					required	: true,
-					pattern		: 'email',
-				},
+				Email	: [
+					{
+						required	: true,
+						msg: 'Необхідно ввести Email'
+					},
+					{
+						pattern		: 'email',
+						msg: 'Слід ввести коректну адресу'
+					}
+				],
 				Address1 : {
 					required	: false,
-					maxLength	: 255
+					maxLength	: 255,
+					msg: 'Значення надто довге (максимум 255 символів)'
 				},
 				Address2 : {
 					required	: false,
-					maxLength	: 255
+					maxLength	: 255,
+					msg: 'Значення надто довге (максимум 255 символів)'
 				},
 				Area	: {
 					required	: false,
-					maxLength	: 255
+					maxLength	: 255,
+					msg: 'Значення надто довге (максимум 255 символів)'
 				},
 				'Zip-code':{
 					required	: false,
-					maxLength	: 255
+					maxLength	: 255,
+					msg: 'Значення надто довге (максимум 255 символів)'
 				},
 				Country : {
 					required	: true,
+					msg: 'Слід вказати країну'
 				},
-				City	: {
-					required	: true,
-					maxLength	: 255
-				}
+				City	: [
+					{
+						required	: true,
+						msg: 'Слід вказати місто'
+					},
+					{
+						maxLength	: 255,
+						msg: 'Значення надто довге (максимум 255 символів)'
+					}
+				]
 			},
 
 
