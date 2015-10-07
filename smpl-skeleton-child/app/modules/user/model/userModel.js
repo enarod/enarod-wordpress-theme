97030eada9c9ad139b443812cb89baee9635ba71
Vasyl Zhuk <vasiliy@boylesoftware.com>
2015-10-07 12:29:54 +0300

    added form sumbit callback, restyled form elements for cabinet

--- smpl-skeleton-child/app/modules/user/model/userModel.js
+++ smpl-skeleton-child/app/modules/user/model/userModel.js
@@ -1,178 +1,201 @@
 define(function(require){
-	'use strict';
+  'use strict';
 
-	var Backbone	= require('backbone'),
-    $   = require('jquery')
-    ,
-	fb				= require('facebook')
-	;
+  var Backbone	= require('backbone'),
+      $   = require('jquery')
+      ,
+      fb				= require('facebook')
+      ;
 
-	return Backbone.Model.extend({
+  return Backbone.Model.extend({
 
-        initialize: function(){
-			FB.init({
-				appId: 770731953025405,
-				version: 'v2.3'
-			});
+    initialize: function(){
+      FB.init({
+        appId: 770731953025405,
+        version: 'v2.3'
+      });
+    },
+
+    getProfile: function(){
+      this.url = BASE_URL+'users/current';
+      this.fetch();
+    },
+
+    validation: {
+      UserEmail	: [
+        {
+          required: true,
+          msg: 'Необхідно ввести Email'
         },
-
-        getProfile: function(){
-            this.url = BASE_URL+'users/current';
-            this.fetch();
+        {
+          pattern: 'email',
+          msg: 'Слід ввести коректну електронну адресу'
+        }
+      ],
+      Password : [
+        {
+          required: true,
+          msg: 'Необхідно вказати пароль'
         },
-
-        validation: {
-			UserEmail	: [
-				{
-					required: true,
-					msg: 'Необхідно ввести Email'
-				},
-				{
-					pattern: 'email',
-					msg: 'Слід ввести коректну електронну адресу'
-				}
-			],
-            Password : [
-                {
-                    required: true,
-					msg: 'Необхідно вказати пароль'
-                },
-                {
-                    pattern: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z)(?=.*[@#_\-$]).{8,100})/,
-					msg: 'Пароль має складатись щонайменш з 8 символів і \
+        {
+          pattern: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z)(?=.*[@#_\-$]).{8,100})/,
+          msg: 'Пароль має складатись щонайменш з 8 символів і \
                         включати одину цифру, одну велику літеру \
                         та один спеціальний символ (-_#@$)'
-                }
-            ],
-            ConfirmPassword : [
-                {
-                    required: true,
-					msg: 'Повторіть введений пароль'
-                },
-                {
-                    equalTo: 'Password',
-					msg: 'Введені значення не співпадають'
-                },
-            ]
-
+        }
+      ],
+      ConfirmPassword : [
+        {
+          required: true,
+          msg: 'Повторіть введений пароль'
         },
-
-        register: function(){
-            this.urlRoot = BASE_URL+'Account/Register';
-            this.save();
+        {
+          equalTo: 'Password',
+          msg: 'Введені значення не співпадають'
         },
+      ]
 
-        /*
-         * Log in using e-mail
-         */
-        signIn: function(){
-            var that = this;
-            $.ajax({
-                method: 'POST',
-                url: BASE_URL+'Account/Signin',
-                contentType: 'application/x-www-form-urlencoded',
-                accepts: 'application/json',
-                data:{
-                    grant_type: 'password',
-                    username: that.get('UserEmail'),
-                    password: that.get('Password')
-                },
-                success: function( data ){
-                    that.logInSuccess(data);
-                },
-                error: function( data ){
-                    that.logInError(data);
-                }           
-            });    
+    },
+
+    register: function(){
+      this.urlRoot = BASE_URL+'Account/Register';
+      this.save();
+    },
+
+    /*
+     * Log in using e-mail
+     */
+    signIn: function(){
+      var that = this;
+      $.ajax({
+        method: 'POST',
+        url: BASE_URL+'Account/Signin',
+        contentType: 'application/x-www-form-urlencoded',
+        accepts: 'application/json',
+        data:{
+          grant_type: 'password',
+          username: that.get('UserEmail'),
+          password: that.get('Password')
         },
-
-        logInSuccess: function(data){
-            this.set({Token : data});
-            var that = this;
-            localStorage.setItem(
-                'edemUser', 
-                JSON.stringify({
-                    'Token'     : that.get('Token'),
-                    'UserEmail' : that.get('UserEmail'),
-                })
-            );
-            this.trigger('loggedIn');
+        success: function( data ){
+          that.logInSuccess(data);
         },
+        error: function( data ){
+          that.logInError(data);
+        }
+      });
+    },
 
-        logInError: function(data){
-            var error = JSON.parse(data.responseText).error_description;
-            this.trigger( 'logInError', { msg: error} );
+    /*
+     * Save user profile
+     */
+    profileSave: function(){
+      var that = this;
+      $.ajax({
+        method: 'PUT',
+        url: BASE_URL+'users/current',
+        contentType: 'application/x-www-form-urlencoded',
+        accepts: 'application/json',
+        data:{
+          username: that.get('UserEmail'),
+          password: that.get('Password')
         },
-
-        getToken: function(){
-            if ( this.get('Token') ){
-                return this.get('Token').token_type+' '
-                    + this.get('Token').access_token;
-            }else{
-                return false;
-            }
+        success: function( data ){
+          //that.logInSuccess(data);
         },
+        error: function( data ){
+          //that.logInError(data);
+        }
+      });
+    },
+
+    logInSuccess: function(data){
+      this.set({Token : data});
+      var that = this;
+      localStorage.setItem(
+          'edemUser',
+          JSON.stringify({
+            'Token'     : that.get('Token'),
+            'UserEmail' : that.get('UserEmail'),
+          })
+      );
+      this.trigger('loggedIn');
+    },
+
+    logInError: function(data){
+      var error = JSON.parse(data.responseText).error_description;
+      this.trigger( 'logInError', { msg: error} );
+    },
+
+    getToken: function(){
+      if ( this.get('Token') ){
+        return this.get('Token').token_type+' '
+            + this.get('Token').access_token;
+      }else{
+        return false;
+      }
+    },
 
 
-        /*
-         * Handling FB user connection
-         */           
-		fbLogIn: function(){
-			var that = this;
+    /*
+     * Handling FB user connection
+     */
+    fbLogIn: function(){
+      var that = this;
 
-			this.initFBConnection();
-			FB.getLoginStatus( function(response){
-				that.statusChangeCallback(response);
-			});
+      this.initFBConnection();
+      FB.getLoginStatus( function(response){
+        that.statusChangeCallback(response);
+      });
 
-		},
+    },
 
-        checkFBLoginState(){
-            var that = this;
-			FB.getLoginStatus( function(response){
-				that.statusChangeCallback(response);
-			});
-        },
+    checkFBLoginState(){
+    var that = this;
+    FB.getLoginStatus( function(response){
+      that.statusChangeCallback(response);
+    });
+  },
 
-		initFBConnection: function(){
-			FB.init({
-				appId: 770731953025405,
-				version: 'v2.3'
-			});
-		},
+  initFBConnection: function(){
+    FB.init({
+      appId: 770731953025405,
+      version: 'v2.3'
+    });
+  },
 
-		statusChangeCallback: function(response){
-			var that = this;
+  statusChangeCallback: function(response){
+    var that = this;
 
-			if (response.status === 'connected' ){
-				FB.api('/me', function(response){
-					//Set User attributes from FB profile
-					that.setUserAttributesFB( response );
-				});
-
-
-			}else if ( response.status === 'not_authorized' ){
-				console.log('Not autorized!');
-
-			}else{
-				FB.login(function(response){
-					that.setUserAttributesFB( response );
-				}, {scope: 'public_profile,email'});
-			}
-
-		},
-
-		setUserAttributesFB: function( response ){
-			this.set({
-				'FirstName' : response.first_name,
-				'LastName'	: response.last_name,
-				'Email'		: response.email,
-			});
-
-			this.trigger('userProfileReady');
-
-		},
+    if (response.status === 'connected' ){
+      FB.api('/me', function(response){
+        //Set User attributes from FB profile
+        that.setUserAttributesFB( response );
+      });
 
 
-	});
+    }else if ( response.status === 'not_authorized' ){
+      console.log('Not autorized!');
+
+    }else{
+      FB.login(function(response){
+        that.setUserAttributesFB( response );
+      }, {scope: 'public_profile,email'});
+    }
+
+  },
+
+  setUserAttributesFB: function( response ){
+    this.set({
+      'FirstName' : response.first_name,
+      'LastName'	: response.last_name,
+      'Email'		: response.email,
+    });
+
+    this.trigger('userProfileReady');
+
+  },
+
+
+});
 });
--- smpl-skeleton-child/app/modules/user/view/userView.js
+++ smpl-skeleton-child/app/modules/user/view/userView.js
@@ -1,180 +1,186 @@
 define(function(require){
-	'use strict';
+  'use strict';
 
-	var $	= require('jquery'),
-	_		= require('underscore'), 
-	Backbone= require('backbone'),
-    stickit = require('stickit'),
-	logInForm	= require('text!modules/user/templates/userLogInForm.html'),
-	registerForm= require('text!modules/user/templates/userRegistrationForm.html'),
-    profileForm = require('text!modules/user/templates/userProfilePage.html')
-	;
+  var $	= require('jquery'),
+      _		= require('underscore'),
+      Backbone= require('backbone'),
+      stickit = require('stickit'),
+      logInForm	= require('text!modules/user/templates/userLogInForm.html'),
+      registerForm= require('text!modules/user/templates/userRegistrationForm.html'),
+      profileForm = require('text!modules/user/templates/userProfilePage.html')
+      ;
 
-	
-	return Backbone.View.extend({
-		
-				className: 'logInSelector',
 
-				initialize: function(data){
-					if (data){
-						this.model = data.model;
-                        this.mode = data.mode;
-                        this.parentView = data.parentView;
-					}
+  return Backbone.View.extend({
 
-                    this.listenTo( this.model, 'loggedIn', this.close );
-                    this.listenTo( this.model, 'logInError', this.logInError );
-                    this.listenTo( this.model, 'sync', this.render); 
-				},
+    className: 'logInSelector',
 
-                addValidation: function(){
-    				Backbone.Validation.bind ( this, {
-    					model	: this.model,
-    					valid	: function( view, attr ){
-    						var el	= view.$('[name='+attr+']'),
-    						group	= el.closest('div.form-element-group');
-    
-    						group.removeClass('has-error');
-    						group.find('.help-block').html('').addClass('hidden');
-    
-    					},
-    					invalid	: function( view, attr, error ){
-    						var el	= view.$('[name='+attr+']'),
-    						group	= el.closest('div.form-element-group');
-    
-    						group.addClass('has-error');
-    						group.find('.help-block').html(error).removeClass('hidden');
-    
-    					}
-    
-    				} );
-                },
+    initialize: function(data){
+      if (data){
+        this.model = data.model;
+        this.mode = data.mode;
+        this.parentView = data.parentView;
+      }
 
-				events: {
-					'click input#fb-login'	: 'fbLogIn',
-					'click input#signIn'	: 'signIn',
-                    'click input#signUp'    : 'signUp',
-                    'click input#register'  : 'register',
-                    'input input#UserEmail' : 'logInInputChanged',
-                    'input input#Password'  : 'logInInputChanged',
-				},
+      this.listenTo( this.model, 'loggedIn', this.close );
+      this.listenTo( this.model, 'logInError', this.logInError );
+      this.listenTo( this.model, 'sync', this.render);
+    },
 
-                bindings: {
-                    '#Password': {
-                        observe: 'Password',
-                        setOptions: {
-                            validate: true
-                        }
-                    
-                    },
-                    '#UserEmail': {
-                        observe: 'UserEmail',
-                        setOptions: {
-                            validate: true
-                        }
-                    },
-                    '#ConfirmPassword': {
-                        observe: 'ConfirmPassword',
-                        setOptions: {
-                            validate: true
-                        }
-                    },
-                },
+    addValidation: function(){
+      Backbone.Validation.bind ( this, {
+        model	: this.model,
+        valid	: function( view, attr ){
+          var el	= view.$('[name='+attr+']'),
+              group	= el.closest('div.form-element-group');
 
-				render: function(){
-					var that = this;
-                    this.setTemplate();
-					$("div.app").append( $(this.el).html( this.template() ) );
+          group.removeClass('has-error');
+          group.find('.help-block').html('').addClass('hidden');
 
-                    if ( this.mode != 'profile' ){
-                        $('.logInSelector').dialog({
-                            modal: true,
-                            height: 380,
-                            width: 450,
-                            closeText: "&times;",
-                            close: function(){
-                                that.close();
-                            }
-                        });
-                    }
+        },
+        invalid	: function( view, attr, error ){
+          var el	= view.$('[name='+attr+']'),
+              group	= el.closest('div.form-element-group');
 
-                    if ( $('#captcha').length ){
+          group.addClass('has-error');
+          group.find('.help-block').html(error).removeClass('hidden');
+
+        }
+
+      } );
+    },
+
+    events: {
+      'click input#fb-login'	: 'fbLogIn',
+      'click input#signIn'	: 'signIn',
+      'click input#signUp'    : 'signUp',
+      'click input#register'  : 'register',
+      'input input#UserEmail' : 'logInInputChanged',
+      'input input#Password'  : 'logInInputChanged',
+      'click input#profileSave'  : 'profileSave'
+    },
+
+    bindings: {
+      '#Password': {
+        observe: 'Password',
+        setOptions: {
+          validate: true
+        }
+
+      },
+      '#UserEmail': {
+        observe: 'UserEmail',
+        setOptions: {
+          validate: true
+        }
+      },
+      '#ConfirmPassword': {
+        observe: 'ConfirmPassword',
+        setOptions: {
+          validate: true
+        }
+      },
+    },
+
+    render: function(){
+      var that = this;
+      this.setTemplate();
+      $("div.app").append( $(this.el).html( this.template() ) );
+
+      if ( this.mode != 'profile' ){
+        $('.logInSelector').dialog({
+          modal: true,
+          height: 380,
+          width: 450,
+          closeText: "&times;",
+          close: function(){
+            that.close();
+          }
+        });
+      }
+
+      if ( $('#captcha').length ){
 //                        this.parentView.addCaptcha('captcha');
-                    }
+      }
 
 //                    FB.XFBML.parse(document.getElementById('fb-login-btn'));
 
-                    this.stickit();
-					return this;
-				},
+      this.stickit();
+      return this;
+    },
 
-                setTemplate: function(){
-                    if ( this.mode == 'logIn' ){
-                        this.template = _.template(logInForm);
-                    }else if ( this.mode == 'register' ) {
-                        this.template = _.template(registerForm);
-                    }else if ( this.mode == 'profile' ){
-                        this.template = _.template(profileForm);
-                    }
-                },
+    setTemplate: function(){
+      if ( this.mode == 'logIn' ){
+        this.template = _.template(logInForm);
+      }else if ( this.mode == 'register' ) {
+        this.template = _.template(registerForm);
+      }else if ( this.mode == 'profile' ){
+        this.template = _.template(profileForm);
+      }
+    },
 
-	
-				fbLogIn: function(){
-					this.model.fbLogIn(); 
-				},
 
-				signIn: function(){
-                    this.model.signIn();
-                    $('#spinner').hide();
-				},
+    fbLogIn: function(){
+      this.model.fbLogIn();
+    },
 
-                signUp: function(){
-					$('.logInSelector').dialog('destroy');
-                    this.mode = 'register';
-                    this.render();
-                },
+    signIn: function(){
+      this.model.signIn();
+      $('#spinner').hide();
+    },
 
-                register: function(){
-                    this.addValidation();
+    signUp: function(){
+      $('.logInSelector').dialog('destroy');
+      this.mode = 'register';
+      this.render();
+    },
 
-    				if ( this.model.isValid(true) ){
-                        $('#spinner').show();
-                        this.listenTo ( this.model, 'sync', this.signIn );
-                        this.model.register();
-    				}
-                },
+    profileSave: function(){
+      this.model.profileSave();
+      $('#spinner').hide();
+    },
 
-                logInInputChanged: function(){
-                    $('#LogInStatus')
-                        .addClass('hidden')
-                        .removeClass('has-error')
-                        .find('.help-block')
-                        .html();
-                        
-                    if (
-                        $('#Password').val().length > 0 &&
-                        $('#UserEmail').val().length > 0
-                    ){
-                        $('#signIn').prop('disabled', false);
-                    }else{
-                        $('#signIn').prop('disabled', true);
-                    }
+    register: function(){
+      this.addValidation();
 
-                },
+      if ( this.model.isValid(true) ){
+        $('#spinner').show();
+        this.listenTo ( this.model, 'sync', this.signIn );
+        this.model.register();
+      }
+    },
 
-                logInError: function(data){
-                    $('#LogInStatus')
-                        .removeClass('hidden')
-                        .addClass('has-error')
-                        .find('.help-block')
-                        .html(data.msg);
-                },
-		
-				close: function(){
-					$('.logInSelector').dialog('destroy');
-					this.remove();
-					this.unbind();
-				},
-	});
+    logInInputChanged: function(){
+      $('#LogInStatus')
+          .addClass('hidden')
+          .removeClass('has-error')
+          .find('.help-block')
+          .html();
+
+      if (
+          $('#Password').val().length > 0 &&
+          $('#UserEmail').val().length > 0
+      ){
+        $('#signIn').prop('disabled', false);
+      }else{
+        $('#signIn').prop('disabled', true);
+      }
+
+    },
+
+    logInError: function(data){
+      $('#LogInStatus')
+          .removeClass('hidden')
+          .addClass('has-error')
+          .find('.help-block')
+          .html(data.msg);
+    },
+
+    close: function(){
+      $('.logInSelector').dialog('destroy');
+      this.remove();
+      this.unbind();
+    }
+  });
 
 });
